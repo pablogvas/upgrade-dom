995fe39fba2e9e4989944eefcc1c83cf4b49339d
PabloGarciaUH <pablo.garcia@bootcamp-upgrade.com>
2023-01-17 21:30:27 +0100

    Iteración #1

---
+++ script.js
@@ -0,0 +1,16 @@
+const button = document.querySelector(".showme");
+console.log(button);
+
+const h1 = document.querySelector("#pillado");
+console.log(h1);
+
+const p = document.querySelectorAll("p");
+console.log(p);
+
+const pokemon = document.querySelectorAll(".pokemon");
+console.log(pokemon);
+
+const atributo = document.querySelectorAll('[data-function="testMe"]');
+console.log(atributo);
+
+console.log(atributo[2]);
